4fd1d0d9ae9229b3d30e8fbdfe2c8cecdc543d84
JJorgeRCastillo <kanh_2832@hotmail.com>
2016-10-12 00:30:43 -0500

    Solucion para Computing Cost, Gradient Descent con una variable para ML

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Machine-Learning-Course
+
+Este repositorio contiene algoritmos que he desarrollado 
+de los ejercicios del curso de Machine Learning de Coursera
+dictado por Mr. Ng Andrew de Stanford University
---
+++ gradientDescent.m
@@ -0,0 +1,43 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+
+
+    % Get the second column values, because first column is ones(n,1)
+    vals0 = X(:,1);
+    vals1 = X(:,2);
+
+    % Calculate the hypothesis value
+
+    hypothesis = (theta(1) .* vals0) + (theta(2) .* vals1);
+
+    % Storage every theta value to be updated on every iteration    
+
+    z_theta = theta(1) - alpha * (1/m) * sum( (hypothesis - y) .* vals0);
+    o_theta = theta(2) - alpha * (1/m) * sum( (hypothesis - y) .* vals1);
+
+    % Save theta values
+    theta = [z_theta; o_theta]; 
+    
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+end
+
+end
